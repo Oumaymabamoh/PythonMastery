85c0959612022469059645bcf0db888275e95dc4
oumaymabamoh <bamoh.oumayma@gmail.cco>
2023-08-16 13:09:00 +0100

    add new file

---
+++ numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+array = np.zeros((3, 4), dtype=int)
+array = np.ones((3, 4), dtype=int)
+array = np.full((3, 4), 5,  dtype=int)
+array = np.random.random((3, 4))
+print(array)
+print(array[0, 0])
